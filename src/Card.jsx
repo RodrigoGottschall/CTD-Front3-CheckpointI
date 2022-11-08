aa9a69d74ad9425a28f1c1b1603dc94ce78c0944
RodrigoGottschall <ro_criscuolo@hotmail.com>
2022-11-07 22:46:49 -0300

    feat card and button components

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,12 @@
 //Este componente deverá receber dados por Props e mostrar as Informações em Tela
 
-export function Card () {
+export function Card (props) {
   return (
-    <div>
-
+    <div className="card-component">
+      <div className="card-body">
+        <h1>{props.productData.name}</h1>
+        <span>R$ {props.productData.price},00</span>
+      </div>
     </div>
   )
 }
