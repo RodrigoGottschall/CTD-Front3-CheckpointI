e58dcc595b3d0f5882ae0e061e41c3dfe0e42d93
RodrigoGottschall <ro_criscuolo@hotmail.com>
2022-11-10 00:02:57 -0300

    feat input pattern

--- src/App.jsx
+++ src/App.jsx
@@ -3,56 +3,26 @@ import { Card } from './Card'
 import './index.css'
 
 // Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
-function validationNameColor(nomeCor) {
-  return nomeCor.trim().length > 3
-}
-
-function validationColorHex(corHexa) {
-  const padrao = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-  let erro = false
-  let haveNumber = false
-
-  if (corHexa.trim().length < 6) {
-    return false
-  }
-
-  corHexa.toUpperCase().split('').map(char => {
-    if (!padrao.includes(char)) {
-      erro = true
-    }
-    haveNumber = haveNumber ? true : !isNaN(Number(char))
-  })
-
-  if (!haveNumber) {
-    erro = true
-  }
-
-  return !erro
-}
-
+// Aqui você irá criar os Estados para manipular os Inputs
 function App() {
-  // Aqui você irá criar os Estados para manipular os Inputs
   const [nameColor, setNameColor] = useState('')
   const [codeColor, setCodeColor] = useState('')
   const [allColors, setAllColors] = useState([])
-  const [formularioErro, setFormularioErro] = useState(false)
 
   function addColor(event) {
     event.preventDefault()
-    let error = false
 
     const newColor = {
       nomeCor: nameColor,
       corHexadecimal: codeColor
     }
 
-    error = !validationNameColor(nameColor) || !validationColorHex(codeColor)
-
-    setFormularioErro(error)
-    if (!error) {
-      setAllColors([...allColors, newColor])
+    if (codeColor.length !== 6 ) {
+      alert('Por favor, verifique os dados inseridos no formulário')
       setNameColor('')
       setCodeColor('')
+    } else {
+      setAllColors([...allColors, newColor])
     }
   }
 
@@ -74,6 +44,7 @@ function App() {
             <label htmlFor="codeColor">Cor</label>
             <input type="text"
               className="form-control"
+              pattern="[a-fA-F0-9]+"
               maxLength="6"
               value={codeColor}
               onChange={event => setCodeColor(event.target.value)}
@@ -84,7 +55,6 @@ function App() {
           <button type="submit">ADICIONAR</button>
         </div>
       </form>
-      <span>{formularioErro ? 'Por favor, verifique os dados inseridos no formulário' : ''}</span>
       <section>
         <h2>Cores Favoritas</h2>
         <div className='all-cards'>
